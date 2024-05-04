a0ec7f6544ec0ca16d2fc0591ddafc4246b09ac5
myselfatul <atulverma.verma4@gmail.com>
2024-05-04 22:34:24 +0530

    added redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { config } from './config/config'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { config } from "./config/config";
+import { useDispatch } from "react-redux";
+import authService from "./core/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <>
-      <h1>Hello Atul {config.appwriteUrl}</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>text</div> : null;
 }
 
-export default App
+export default App;
